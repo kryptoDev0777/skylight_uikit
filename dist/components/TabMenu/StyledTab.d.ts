3c8eb606ee52b0170767dc149aeea6fe56813e44
Richard382 <yaroslav0333@outlook.com>
2021-11-29 21:36:17 +1000

    'skylight'

---
+++ dist/components/TabMenu/StyledTab.d.ts
@@ -0,0 +1,6 @@
+interface StyledTabProps {
+    color: "backgroundAlt" | "textSubtle";
+    bgColor: "textSubtle" | "input";
+}
+declare const StyledTab: import("styled-components").StyledComponent<"button", import("styled-components").DefaultTheme, StyledTabProps, never>;
+export default StyledTab;
